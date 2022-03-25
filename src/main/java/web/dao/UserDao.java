3e66d8f99aac5be06d9ae27afccdbe474cf29d62
User <minass1@mail.ru>
2022-03-25 18:03:25 +0700

    4

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,29 @@
+package web.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+  //  @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+   // @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+}
+
